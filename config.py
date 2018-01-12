21ff68b0c9111a29e38c4907848cef57bc37fd3a
Izzy Brand <isaiah_brand@brown.edu>
2018-01-11 23:33:50 -0500

    set params for new test

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 PORT 					= '/dev/tty.usbmodem1'	# whic port the pixhawk is on
 BURN_PIN 				= 27				# pin with burn relay
-BURN_ALTITUDE 			= 10 			    # altitude at which to burn
-BURN_TIME_ABOVE 		= 10				# time above burn alt before ignite
+BURN_ALTITUDE 			= 85 			    # altitude at which to burn
+BURN_TIME_ABOVE 		= 5					# time above burn alt before ignite
 BURN_TIME_BELOW			= 3					# time below burn alt after ignite
-TARGET_LAT	 			= 42.345131			# desired landing latitude
-TARGET_LON				= -71.210126		# desired landing longitude
+TARGET_LAT	 			= 42.3455724		# desired landing latitude
+TARGET_LON				= -71.2100037		# desired landing longitude
 TARGET_ALT 				= 0 				# meters above sea level
 LOOP_DELAY				= 0.5				# time in seconds to delay within loops
 MIN_SATS				= 5					# minimum number of satellites before vehicle takes off
